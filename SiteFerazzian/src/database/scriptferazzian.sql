1fa99c0ddf2c11760597050901904870f508f88c
Joao-Nocee <joao.noce@sptech.school>
2024-06-08 00:10:56 -0300

    Alterações no Banco de Dados

--- SiteFerazzian/src/database/scriptferazzian.sql
+++ SiteFerazzian/src/database/scriptferazzian.sql
@@ -2,35 +2,49 @@
 create database ferazzian;
 use ferazzian;
 
+SELECT idDadosSensor, sensorTemp, sensorUmid, DATE_FORMAT(horaColeta,'%H:%i:%s') as horaColeta
+     FROM dadosSensor WHERE fkSensorDados = 1 LIMIT 7;
+     
+     SELECT sensorTemp, sensorUmid, DATE_FORMAT(horaColeta,'%H:%i:%s') as horaColeta
+     FROM dadosSensor join sensor on fkSensorDados = idSensor WHERE idSensor = 1 and fkSensorFazenda = 1;
+     
+     select * from sensor;
+     select * from dadosSensor;
+     select * from fazenda;
+     
 CREATE TABLE empresa (
   idEmpresa INT PRIMARY KEY AUTO_INCREMENT,
   nomeEmpresa VARCHAR(45),
   cep CHAR(9),
   cnpj CHAR(18),
-  numero VARCHAR(45));
+  numero VARCHAR(45),
+  telefone CHAR(12));
 
 INSERT INTO empresa VALUES
-(default, 'Fazenda Bela Vista','12345-678','26.186.289/0001-79','122'),
-(default, 'Fazenda Vista Verde','87654-321','26.186.289/0001-78','143'),
-(default, 'Fazenda Santa Rita','54321-876','26.186.289/0001-77','333');
-
+(default, 'Fazenda Bela Vista','12345-678','26.186.289/0001-79','122', '11961713435'),
+(default, 'Fazenda Vista Verde','87654-321','26.186.289/0001-78','143','11962719090'),
+(default, 'Fazenda Santa Rita','54321-876','26.186.289/0001-77','333','11966723334');
 
 CREATE TABLE usuario (
   idUsuario INT UNIQUE auto_increment,
   nomeUsuario VARCHAR(45),
   email VARCHAR(30),
-  telefone VARCHAR(12),
   senha VARCHAR(45),
   cpf CHAR(11),
+  tipoUsuario VARCHAR(45),
   fkUsuarioEmpresa INT, 
   PRIMARY KEY (idUsuario, fkUsuarioEmpresa),
   FOREIGN KEY (fkUsuarioEmpresa)
   REFERENCES empresa(idEmpresa));
+  
 
-INSERT INTO usuario (nomeUsuario, email, telefone, senha, cpf, fkUsuarioEmpresa) VALUES
-('ANTONIO','antonio@belavista.com','11961713435','76hf238rB', '46464798833',1),
-('RENAN','renan@vistaverde.com','11962719090','sdh586T', '46464795522', 2),
-('PEDRO','pedro@santarita.com','11966723334','Hrfer3412', '46464782266',3);
+INSERT INTO usuario (nomeUsuario, email, senha, cpf, fkUsuarioEmpresa) VALUES
+('ANTONIO','antonio@belavista.com','76hf238rB', '46464798833',1),
+('RENAN','renan@vistaverde.com','sdh586T', '46464795522', 2),
+('PEDRO','pedro@santarita.com','Hrfer3412', '46464782266',3);
+
+INSERT INTO usuario (nomeUsuario, email, senha, cpf, fkUsuarioEmpresa) VALUES
+('JOÃO','jpmorenoce@gmail.com','190406', '40790791846',3);
 
 CREATE TABLE fazenda (
   idFazenda INT PRIMARY KEY AUTO_INCREMENT,
@@ -38,6 +52,7 @@ CREATE TABLE fazenda (
   tipoSoja VARCHAR(45),
   tamHectare INT,
   fkEmpresaFazenda INT, CONSTRAINT fkEmpFazenda FOREIGN KEY (fkEmpresaFazenda) REFERENCES empresa(idEmpresa));
+
 
 INSERT INTO fazenda VALUES
 (null,'A','Soja Orgânica', 2, 1),
@@ -47,13 +62,29 @@ INSERT INTO fazenda VALUES
 CREATE TABLE parametros (
 	idParametro INT AUTO_INCREMENT,
     tempMaxima DECIMAL,
-    tempoMinima DECIMAL,
-    umidMaxima DECIMAL,
+    tempMinima DECIMAL,
     umidMinima DECIMAL,
     fkParametroFazenda INT,
     PRIMARY KEY (idParametro, fkParametroFazenda), FOREIGN KEY (fkParametroFazenda) REFERENCES fazenda(idFazenda)
 );
 
+insert into parametros values
+(null, 28, 18, 80, 1),
+(null, 30, 20, 70, 2),
+(null, 26, 22, 90, 3);
+
+SELECT tempMaxima,
+    tempMinima,
+    umidMinima,
+    sensorTemp,
+    sensorUmid,
+    DATE_FORMAT(horaColeta,'%H:%i:%s') as horaColeta
+    FROM dadosSensor
+    join sensor on fkSensorDados = idSensor
+    join fazenda on fkSensorFazenda = idFazenda
+    join parametros on fkParametroFazenda = idFazenda
+    WHERE idSensor = 3 order by horaColeta desc limit 1;
+select * from parametros;
 CREATE TABLE sensor (
   idSensor INT PRIMARY KEY AUTO_INCREMENT,
   nome VARCHAR(45),
@@ -76,6 +107,26 @@ SELECT * FROM usuario;
 SELECT * FROM fazenda;
 SELECT * FROM sensor;
 SELECT * FROM dadosSensor;
+
+insert into dadosSensor values
+(null, 17, 64, default, 1),
+(null, 39, 85, default, 2),
+(null, 24, 91, default, 3);
+select * from dadosSensor;
+
+SELECT tempMaxima, tempMinima, umidMinima, sensorTemp, sensorUmid, DATE_FORMAT(horaColeta,'%H:%i:%s') as horaColeta
+     FROM dadosSensor join sensor on fkSensorDados = idSensor join fazenda on fkSensorFazenda = idFazenda join parametros on fkParametroFazenda = idFazenda WHERE idSensor = 1 limit 7;
+
+
+
+
+
+
+
+
+
+
+
 
 -- EXIBINDO O NOME DA EMPRESA, TIPO DE SOJA, DATAS DO PLANTIO E COLHEITA, NOME DO SENSOR, TEMPERATURAS E UMIDADES REGISTRADAS, E A HORA DESSE REGISTRO
 
